4aa26c43246adc07fc7a373da660f9c92b872f77
Apple <Apple@iMac-Apple.local>
2020-05-04 19:59:26 +0300

    version 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Рассчет закона Ома");
+        System.out.println("\n");
+        System.out.println("Что рассчитываем?");
+        System.out.println("1. Напряжение");
+        System.out.println("2. Сопротивление");
+        System.out.println("3. Ток");
+        System.out.println("Введите цифру:");
+
+        //Принимаем выбор от пользователя
+        Scanner num = new Scanner(System.in);
+        int vyb = num.nextInt(); //объявляем переменную и записываем в нее ввод пользователя
+
+        float u, r, a;
+
+        switch (vyb) {
+            case 1:
+                System.out.println("Введите сопротивление:");
+                Scanner num2 = new Scanner(System.in);
+                r = num2.nextInt();
+                System.out.println("Введите ток:");
+                Scanner num3 = new Scanner(System.in);
+                a = num3.nextInt();
+                u = r * a;
+                System.out.println("Напряжение = " + u);
+                break;
+            case 2:
+                System.out.println("Введите напряжение:");
+                Scanner num4 = new Scanner(System.in);
+                u = num4.nextInt();
+                System.out.println("Введите ток:");
+                Scanner num5 = new Scanner(System.in);
+                a = num5.nextInt();
+                r = u / a;
+                System.out.println("Сопротивление = " + r);
+                break;
+            default:
+                System.out.println("Неправильный ввод!");
+        }
+    }
+}
