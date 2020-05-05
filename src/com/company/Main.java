36a64cbe7f770ff6eb6f7bf76ffa352cb5cd8078
ah1102 <ah1102@yandex.ru>
2020-05-05 17:53:30 +0300

    Delete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,10 @@ public class Main {
         System.out.println("1. Напряжение");
         System.out.println("2. Сопротивление");
         System.out.println("3. Ток");
+        System.out.println();
+        System.out.println("4. Выход");
         System.out.println("Введите цифру:");
+
 
         //Принимаем выбор от пользователя
         Scanner num = new Scanner(System.in);
@@ -22,36 +25,32 @@ public class Main {
         switch (vyb) {
             case 1: //расчет напряжения
                 System.out.println("Введите сопротивление:");
-                Scanner num2 = new Scanner(System.in);
-                r = num2.nextInt();
+                r = num.nextInt();
                 System.out.println("Введите ток:");
-                Scanner num3 = new Scanner(System.in);
-                a = num3.nextInt();
+                a = num.nextInt();
                 u = r * a;
                 System.out.println("Напряжение = " + u);
                 break;
             case 2: //расчет сопротивления
                 System.out.println("Введите напряжение:");
-                Scanner num4 = new Scanner(System.in);
-                u = num4.nextInt();
+                u = num.nextInt();
                 System.out.println("Введите ток:");
-                Scanner num5 = new Scanner(System.in);
-                a = num5.nextInt();
+                a = num.nextInt();
                 r = u / a;
                 System.out.println("Сопротивление = " + r);
                 break;
             case 3: //расчет тока
                 System.out.println("Введите напряжение:");
-                Scanner num6 = new Scanner(System.in);
-                u = num6.nextInt();
+                u = num.nextInt();
                 System.out.println("Введите сопротивление:");
-                Scanner num7 = new Scanner(System.in);
-                r = num7.nextInt();
+                r = num.nextInt();
                 a = u / r;
                 System.out.println("Сопротивление = " + a);
                 break;
             default:
                 System.out.println("Неправильный ввод!");
         }
+
+
     }
 }
